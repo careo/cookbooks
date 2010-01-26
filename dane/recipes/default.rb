6727c7bf4a866c25814ec80499fdecb3f79133e9
Dane Jensen <dane.jensen@gmail.com>
2010-01-25 20:46:59 -0800

    rake metadata

--- dane/recipes/default.rb
+++ dane/recipes/default.rb
@@ -1,10 +1,16 @@
 package "zsh"
 
+
+group "admin" do
+  gid 1000
+end
+
 user "djensen" do
   comment "djensen"
-  uid "1000"
-  gid "djensen"
+  uid 1000
+  gid 1000
   home "/home/djensen"
   shell "/usr/bin/zsh"
   #password "$1$JJsvHslV$szsCjVEroftprNn4JHtDi."
 end
+
