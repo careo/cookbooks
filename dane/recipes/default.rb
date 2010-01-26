6f56f0d92fee16ba7fd8186a1bf57542f01f1c60
Dane Jensen <dane.jensen@gmail.com>
2010-01-25 21:00:12 -0800

    just drop my 'development tweaks' rightscript in there for now

--- dane/recipes/default.rb
+++ dane/recipes/default.rb
@@ -14,3 +14,28 @@ user "djensen" do
   #password "$1$JJsvHslV$szsCjVEroftprNn4JHtDi."
 end
 
+
+bash "do stuff" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+  # fix those "4gb seg fixup" errors
+  echo 'hwcap 0 nosegneg' > /etc/ld.so.conf.d/libc6-xen.conf && ldconfig
+
+
+  mkdir /opt/repos
+  cd /opt/repos
+  git clone git://github.com/careo/ubuntu-rightimage-misc.git
+  cd /opt/repos/ubuntu-rightimage-misc
+
+  # add sources
+  #cat sources.list >> /etc/apt/sources.list
+  apt-get update
+
+  # setup zsh
+  cp zshrc ~/.zshrc
+  sed -i /etc/passwd -e 's/root:x:0:0:root:\/root:\/bin\/bash/root:x:0:0:root:\/root:\/usr\/bin\/zsh/'
+  EOH
+end
+
+
