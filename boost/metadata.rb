3cd9cab4b5e5ad264d2fbb935cec9d13b0fda74c
Dane Jensen <dane.jensen@gmail.com>
2010-01-25 23:10:28 -0800

    attempt to fix ruby_enterprise

--- boost/metadata.rb
+++ boost/metadata.rb
@@ -7,3 +7,29 @@ version          "0.1"
 recipe           "boost", "Installs libboost-dev"
 
 supports "ubuntu"
+
+attribute "ruby_enterprise",
+  :display_name => "Ruby Enterprise Edition options",
+  :type => "hash"
+  
+
+attribute "ruby_enterprise/install_path",
+    :display_name => "Install Path",
+    :default => "/opt/ruby-enterprise"
+
+attribute "ruby_enterprise/ruby_bin",
+    :display_name =>  "Ruby bin",
+    :default => "/opt/ruby-enterprise/bin/ruby"
+
+attribute "ruby_enterprise/gems_dir",
+    :display_name => "Gems Dir",
+    :default => "/opt/ruby-enterprise/lib/ruby/gems/1.8"
+
+attribute "ruby_enterprise/version",
+    :display_name => "Version",
+    :default => '1.8.7-2009.10'
+
+attribute "ruby_enterprise/url",
+    :display_name => "url",
+    :default => "http://rubyforge.org/frs/download.php/66162/ruby-enterprise-1.8.7-2009.10"
+    
